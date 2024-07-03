f5ab4b53a6e34761efef7c0980289ae78ddfd03d
cmk05R <cfvhca@gmail.com>
2024-07-03 06:18:49 -0300

    add: vistas agregadas

---
+++ app/src/main/java/com/example/seprice5/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.seprice5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.seprice5.databinding.HomeBinding
+import com.example.seprice5.ui.theme.SePrice5Theme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding : HomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = HomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+
+
+
+        // Llamada a setContent para contenido Compose
+        setContent {
+            SePrice5Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SePrice5Theme {
+        Greeting("Android")
+    }
+}
